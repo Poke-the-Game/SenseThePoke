2a0cd9c34820db9b2a512f6873bc1a1125adeaa3
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2018-11-04 03:58:43 +0100

    Implement external hardware controller

---
+++ src/config.js
@@ -0,0 +1,5 @@
+let config = {
+  ws_url: 'ws://10.42.0.154:1337'
+}
+
+export default config
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Phaser from 'phaser'
+
+import config from './config'
 import TerrainGenerator from './terrain'
 
-let config = {
+let gameConfig = {
   type: Phaser.AUTO,
   parent: 'pokethegame',
   width: 800,
@@ -19,7 +21,9 @@ let config = {
   }
 }
 
-let game = new Phaser.Game(config)
+let game = new Phaser.Game(gameConfig)
+let positionX = 50
+let positionY = 300
 
 function preload () {
   this.load.image('player', 'assets/player.png')
@@ -30,14 +34,52 @@ function create () {
   this.cursors = this.input.keyboard.createCursorKeys()
   this.matter.world.setBounds().disableGravity()
 
-  this.player = this.matter.add.image(50, 300, 'player')
+  this.player = this.matter.add.image(50, positionX, 'player')
 
   this.terrain = new TerrainGenerator(game, this)
   this.terrain.nextLine()
+
+  let ws = new window.WebSocket(config.ws_url)
+  this.data_chain = []
+  ws.addEventListener('message', ({ data }) => {
+    data = parseFloat(data)
+    if (data === 0) {
+      return
+    }
+
+    let avg = -1
+    let num = 10
+    if (this.data_chain.length < num) {
+      this.data_chain.push(data)
+      console.log(`Waiting for more data ${this.data_chain.length}/${num}`)
+    } else {
+      let sum = this.data_chain.reduce(
+        (previous, current) => current + previous, 0)
+      avg = sum / this.data_chain.length
+    }
+
+    if (avg === -1) {
+      return
+    }
+    if (this.cursors.up.isDown || this.cursors.down.isDown) {
+      return
+    }
+
+    let diff = data - avg
+
+    let minY = -8
+    let maxY = 8
+    let velocityY = Math.min(Math.max(minY, diff), maxY)
+    velocityY *= -1
+
+    this.player.setVelocityY(velocityY)
+    // console.log(avg, data, diff, velocityY)
+  })
 }
 
 function update () {
-  this.player.setVelocity(0)
+  this.player.setPosition(
+    positionX, this.player.body.position.y)
 
   if (this.cursors.up.isDown) {
     this.player.setVelocityY(-8)
